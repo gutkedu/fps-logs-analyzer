63ce8e48ce3ee02ebdda13d138db74c6db2c7747
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2025-07-28 13:10:25 -0300

    feat: implement in-memory players repository with CRUD operations

---
+++ test/repositories/in-memory-players-repository.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { Player } from '@/app/entities/player';
+import { PlayersRepository } from '@/app/repositories/players-repository';
+
+export class InMemoryPlayersRepository implements PlayersRepository {
+  public players: Player[] = [];
+
+  async create(player: Player): Promise<void> {
+    this.players.push(player);
+  }
+
+  async createMany(players: Player[]): Promise<void> {
+    this.players.push(...players);
+  }
+
+  async findById(playerId: string): Promise<Player | null> {
+    const player = this.players.find((item) => item.id === playerId);
+
+    if (!player) {
+      return null;
+    }
+
+    return player;
+  }
+
+  async findByName(name: string): Promise<Player | null> {
+    const player = this.players.find((item) => item.name === name);
+
+    if (!player) {
+      return null;
+    }
+
+    return player;
+  }
+
+  async findMany(): Promise<Player[]> {
+    return this.players;
+  }
+}
